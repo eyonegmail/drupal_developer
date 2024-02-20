4020c0c2c95ae7367a06806878ab70b25cdb0830
Emi Yonekawa <domolove@gmail.com>
2024-02-20 21:01:39 +0900

    JSでDrupal.behaviors、once対応

--- web/themes/custom/simplesmart/js/script.js
+++ web/themes/custom/simplesmart/js/script.js
@@ -1,5 +1,11 @@
-var currentUser = drupalSettings.simplesmart.currentUser;
+Drupal.behaviors.myBehavior = {
+  attach: function (context, settings) {
+    once('myBehavior', 'html').forEach(function () {
+      var currentUser = drupalSettings.simplesmart.currentUser;
 
-console.log("ユーザー名：", currentUser.username);
-console.log("役割一覧：", currentUser.roles);
-console.log("タイムゾーン：", currentUser.timezone);
+      console.log("ユーザー名：", currentUser.username);
+      console.log("役割一覧：", currentUser.roles);
+      console.log("タイムゾーン：", currentUser.timezone);
+    })
+  }
+}
