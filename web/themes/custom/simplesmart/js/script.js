b3c8fb18e189fa5a2d5dbec99f6ac45733d4db47
Emi Yonekawa <domolove@gmail.com>
2024-02-20 21:22:43 +0900

    JS修正

--- web/themes/custom/simplesmart/js/script.js
+++ web/themes/custom/simplesmart/js/script.js
@@ -6,6 +6,6 @@ Drupal.behaviors.myBehavior = {
       console.log("ユーザー名：", currentUser.username);
       console.log("役割一覧：", currentUser.roles);
       console.log("タイムゾーン：", currentUser.timezone);
-    })
+    });
   }
-}
+};
